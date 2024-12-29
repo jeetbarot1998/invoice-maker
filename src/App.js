046b4bdb3234e7e2761701d13227109a3907a70c
jeetbarot1998 <jeetbarot1998@gmail.com>
2024-12-29 11:16:47 +0530

    code refactor

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import PDFShareComponent from './components/PDFShareComponent';
+// import PDFShareComponent from './components/PDFShareComponent';
 import InvoiceForm from './components/InvoiceForm';
 
 
